8f08c8da7919d2379df427a334b21b1f8450734d
xzc1999 <xuzhecheng7@gmail.com>
2021-04-15 11:59:30 +0800

    new

--- flask_project1.0/db.py
+++ flask_project1.0/db.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, abort, make_response
 import pymysql
 app=Flask(__name__) #创建1个Flask实例
-#test
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:xzc19991208@localhost/rg"
 #指定当视图执行完毕后,自动提交数据库操作
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -167,5 +167,5 @@ def deletecard(name,roomid,password):
   mydb.session.commit()
   result = {'msg': '已删除'}
   return jsonify(result)
-if __name__ == '__main__':
-  app.run()     
+#if __name__ == '__main__':
+  #app.run()     
